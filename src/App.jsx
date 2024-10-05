3855ff1d8e6d512f3fa4fd1e2835cdd684c9b57d
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-10-05 12:02:11 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-// import { useState, useEffect } from "react";
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 import "./App.css";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm />
-        <SearchBox />
-        <ContactList />
-      </div>
-    </Provider>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
   );
 };
 
