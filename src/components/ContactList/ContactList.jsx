77ee3a45a613bef15aa216fb3be2c108698dd4e8
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-10-13 13:17:24 +0300

    5

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,12 +2,18 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectContacts } from "../../redux/contactsSlice";
 import { fetchContacts } from "../../redux/contactsOps";
+import { selectNameFilter } from "../../redux/filtersSlice";
 import Contact from "../Contact/Contact";
 import s from "./ContactList.module.css";
 
 const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -16,7 +22,7 @@ const ContactList = () => {
   return (
     <div className={s.container}>
       <ul className={s.element}>
-        {contacts.map((contact) => (
+        {filteredContacts.map((contact) => (
           <li className={s.item} key={contact.id}>
             <Contact
               name={contact.name}
