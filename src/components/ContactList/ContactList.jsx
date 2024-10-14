7376cdbfea3fec11ded498719b4c209f0b436874
Olena-Synytsia <alenasinitsa85@gmail.com>
2024-10-14 13:02:31 +0300

    9

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,21 +1,14 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectFilteredContacts } from "../../redux/contactsSlice";
-import { fetchContacts } from "../../redux/contactsOps";
 import { selectLoading, selectError } from "../../redux/contactsSlice";
 import Contact from "../Contact/Contact";
 import s from "./ContactList.module.css";
 
 const ContactList = () => {
-  const dispatch = useDispatch();
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
 
   const filteredContacts = useSelector(selectFilteredContacts);
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
 
   return (
     <div className={s.container}>
